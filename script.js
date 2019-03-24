113f762e2a81cd73d0b07fa7439472530601c130
Mariusz <szczerbal.mariusz@gmail.com>
2019-03-24 06:29:27 +0100

    adding scroll smoothing

--- script.js
+++ script.js
@@ -3,4 +3,15 @@ $(document).ready(function() {
         $(this).toggleClass('active');
         $('nav').toggleClass('active');
     });
+
+    $('nav ul li a').click(function() {
+        let getElement = $(this).attr('href');
+        if($(getElement).length) {
+            let getOffset = $(getElement).offset().top;
+            $('html, body').animate({
+                scrollTop: getOffset - 100
+            }, 500);
+        }
+        return false;
+    });
 });
